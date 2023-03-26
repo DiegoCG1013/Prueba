d386276884d407db63ca8c6ff2d6e481223354a1
Diego <diegocarrasco1013@gmail.com>
2023-03-26 23:47:37 +0200

    Metodos para MCD e intento de metodo con lambda

---
+++ src/main/java/MCD.java
@@ -0,0 +1,30 @@
+import java.util.function.IntBinaryOperator;
+
+public class MCD {
+
+    int mcdEuclidesRecursivo(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return mcdEuclidesRecursivo(b, a % b);
+        }
+    }
+
+    int mcdEuclidesIterativo(int a, int b) {
+        while (b!= 0) {
+            int aux = b;
+            b = a % b;
+            a = aux;
+        }
+        return a;
+    }
+
+    static int mcdEuclidesLambda(int x, int y) {
+        IntBinaryOperator mcd = null;
+        mcd = (a, b) -> b == 0 ? a : mcd.applyAsInt(b, a % b);
+        return mcd.applyAsInt(x, y);
+    }
+            public static void main(String[] args) {
+        System.out.println(mcdEuclidesLambda(12, 18));
+    }
+}
